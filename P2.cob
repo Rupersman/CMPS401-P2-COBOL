0f357d898531d6c579555146875609e6c23f22d1
Rupersman <reecebourgeois@gmail.com>
2021-09-21 16:39:33 -0500

    hello world from Pao's notes

---
+++ P2.cob
@@ -0,0 +1,22 @@
+       *>  Your program reads an input file (P2In.dat). It shows the 
+       *>  outputs on an output file (P2Out.dat) and on the screen. 
+       *>  The following headings appear on your outputs:
+       *>  a. Your name
+       *>  b. Student W number
+       *>  c. COURSE TITLE, GR, EARNED, QPTS
+       *>  d. SEMESTER
+       *>  e. CUMULATIVE
+
+       *>  Program-ID: P2.cob
+       *>  Author: Paogrammers
+       *>  OS: Ubuntu 20
+       *>  Compiler: OpenCOBOL
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. P2.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello".
+           STOP RUN.
+      
+       *>  Input: No
+       *>  Output:
+       *>  Hello
