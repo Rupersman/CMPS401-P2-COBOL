90b8bac3f7679b5e5ea99fd1001d4dbed5263cf6
TimMcR <31673072+TimMcR@users.noreply.github.com>
2021-09-26 15:43:08 -0500

    Output working

--- P2.cob
+++ P2.cob
@@ -38,6 +38,7 @@
        
        FD myOutFile.
        01 outRecord.
+           02 txt PIC X(71).
            02 finalLine2 PIC X.
 
        WORKING-STORAGE SECTION.
@@ -53,50 +54,109 @@
            01 semQpts PIC 99.
            01 cumQpts PIC 999.
            01 semGPA PIC 9.99.
-           01 cumGPA pic 9.99.
+           01 cumGPA PIC 9.99.
       
+           01 head1 PIC X(30) VALUE "                       SOUTHEA".
+           01 head2 PIC X(26) VALUE "STERN LOUISIANA UNIVERSITY".
+           01 head3 PIC X(31) VALUE "                               ".
+           01 head4 PIC X(17) VALUE "HAMMOND, LA 70402".
 
        PROCEDURE DIVISION.
            OPEN INPUT myInFile.
            OPEN OUTPUT myOutFile.
 
            PERFORM subRead *> First subread
+           MOVE finalLine to finalLine2
            *>Main heading
-           DISPLAY "                       SOUTHEA" WITH NO ADVANCING
-           DISPLAY "STERN LOUISIANA UNIVERSITY"
-           Display "                              " WITH NO ADVANCING
-           Display " HAMMOND, LA 70402 "
+           DISPLAY head1, head2 
 
+           STRING  head1
+                   head2
+                   "                      "
+           into txt
+           END-STRING
+           WRITE outRecord 
+
+           Display head3, head4
+           
+           MOVE " " to txt
+           STRING  head3
+                   head4
+                   "       "
+           into txt
+           END-STRING
+           WRITE outRecord
            
            DISPLAY sName
+           
+           MOVE " " to txt
+           STRING  sName
+                   "             "
+           into txt
+           END-STRING
+           WRITE outRecord 
+
            DISPLAY wNum
-           DISPLAY ""
+
+           MOVE " " to txt
+           STRING  wNum
+                   "                "
+                       finalLine2
+           into txt
+           END-STRING
+           WRITE outRecord
+
+           DISPLAY " "
+
+           *>WRITE outRecord from finalLine2
+
            MOVE sem to currSem
            DISPLAY currSem
-           DISPLAY "COURSE    TITLE               " WITH NO ADVANCING
+
+           MOVE " " to txt
+           STRING  currSem
+                   "                "
+           into txt
+           END-STRING
+           WRITE outRecord
 
            *>Display heading
-           DISPLAY "        GR    EARNED      QPTS"
+           DISPLAY "COURSE    TITLE               " WITH NO ADVANCING
+           DISPLAY "         GR    EARNED      QPTS"
+           
+           MOVE " " to txt
+           STRING  "COURSE    TITLE               "
+                   "         GR    EARNED      QPTS"
+           into txt
+           END-STRING
+           WRITE outRecord
 
            PERFORM UNTIL w = "NO"
                *> Loop block
                *> Perform functions and display
+               
                IF sem IS  NOT EQUAL TO currSem
                    *>If we have read in a new semester display the 
                    *>semester and cumulative credits, qpts, and gpa
                    PERFORM finishSemester
                    MOVE sem to currSem
-                   DISPLAY ""
+                   DISPLAY " "
                    DISPLAY sem
+
+                   MOVE " " to txt
+                   STRING  finalLine2
+                           sem
+                           "                "
+                   into txt
+                   END-STRING
+                   WRITE outRecord
                END-IF
-
-               PERFORM semesterDisplay
-
-               MOVE finalLine to finalLine2
-               WRITE outRecord
-               PERFORM subRead
+           
+               PERFORM semesterDisplay    
                
+               PERFORM subRead
            END-PERFORM.
+
            PERFORM finishSemester
            CLOSE myInFile.
            CLOSE myOutFile.
@@ -111,8 +171,6 @@
        *>Displays the semester class 
        semesterDisplay.
            *>Calculate qpts
-           DISPLAY course, cTitle, " ", grade, "     " WITH NO ADVANCING
-           DISPLAY credits, ".00        " WITH NO ADVANCING
            IF grade IS EQUAL TO "A"
                MOVE 4 to gradeFac
            ELSE IF grade IS EQUAL TO "B"
@@ -122,10 +180,27 @@
            Else 
                MOVE 1 to gradeFac
            END-IF.
-           
+
            COMPUTE qpts = gradeFac * credits.
+
+           DISPLAY course, cTitle, " ", grade, "     " WITH NO ADVANCING
+           DISPLAY credits, ".00        " WITH NO ADVANCING
            DISPLAY qpts, ".00".
            
+           MOVE " " to txt
+           STRING  course
+                   cTitle
+                   " "
+                   grade
+                   "     "
+                   credits
+                   ".00        "
+                   qpts
+                   ".00"
+           into txt
+           END-STRING
+           WRITE outRecord
+
            COMPUTE semEarned = semEarned + credits.
            COMPUTE semQpts = semQpts + qpts.
            
@@ -133,20 +208,44 @@
        *>If the next semester is different from the previous one, then 
        *>display the total semester and cummulative points and 
        *>calculate the semester and cummulative gpa
-       *>
+
        finishSemester.
            COMPUTE cumEarned = cumEarned + semEarned.
            COMPUTE cumQpts = cumQpts + semQpts.
+           COMPUTE semGPA = semQpts / semEarned.
+           COMPUTE cumGPA = cumQpts / cumEarned.
            DISPLAY "                         SEMESTER" WITH NO ADVANCING
            DISPLAY "            ", semEarned, ".00" WITH NO ADVANCING
            DISPLAY "       ", semQpts, ".00     " WITH NO ADVANCING
-           COMPUTE semGPA = semQpts / semEarned.
            DISPLAY semGPA
+
+           MOVE " " to txt
+           STRING  "                         SEMESTER"
+                   "            "
+                   semEarned
+                   ".00       "
+                   semQpts
+                   ".00     "
+           into txt
+           END-STRING
+           WRITE outRecord
+
            DISPLAY "                         " WITH NO ADVANCING
            DISPLAY "CUMMULATIVE         " WITH NO ADVANCING
            DISPLAY cumEarned, ".00      " WITH NO ADVANCING
            DISPLAY cumQpts, ".00     " WITH NO ADVANCING
-           COMPUTE cumGPA = cumQpts / cumEarned.
            DISPLAY cumGPA.
+
+           MOVE " " to txt
+           STRING  "                         CUMMULATIVE         "
+                   cumEarned
+                   ".00      "
+                   cumQpts
+                   ".00     "
+                   cumGPA
+           into txt
+           END-STRING
+           WRITE outRecord
+
            MOVE 0 to semEarned.
            MOVE 0 to semQpts.
